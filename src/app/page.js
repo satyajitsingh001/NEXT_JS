3396202a84bfc07f19da38538cacf6fcdee2ad08
satyajitsingh001 <satyajitsingh005@gmail.com>
2023-10-20 17:43:18 +0530

    css-Add

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Style and CSS with Next js</h1>
+      <h2>Heading 2 in main page</h2>
+    
+    </main>
+  )
+}
+
+
