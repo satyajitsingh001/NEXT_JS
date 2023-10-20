8b9396bed644b34fcc3a5b181e1f9c3acf37c6c2
satyajitsingh001 <satyajitsingh005@gmail.com>
2023-10-20 17:46:47 +0530

    Css-Module

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import custom from './custom.module.css'
+import other from  './other.module.css'
+import outside from '@/style/outside.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={custom.main} >CSS Modules with Next js</h1>
+      <h2 className={other.main} >CSS Modules with Next js</h2>
+      <h2 className={outside.main} >Outside css</h2>
+    </main>
+  )
+}
+
+
+
